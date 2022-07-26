051b79a3d4aca73e0cf764e91c47b1c1c9df6fcc
Andriy Konoval <ceperiv@dmail.com>
2022-07-26 10:22:30 +0300

    footer turned to the components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Button, Cars} from "./components";
+import {Button, Cars, Footer} from "./components";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
             <Button/>
             <hr/>
             <Cars/>
-            <footer>all rights reserved</footer>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../components.style.css'
+
+
+function Footer(props) {
+    return (
+        <footer>
+            <span>all rights reserved</span>
+        </footer>
+    );
+}
+
+export {Footer};
